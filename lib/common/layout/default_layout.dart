6a15b645ef3fe3fb7788cdbe280e5bbe0db46f67
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2023-02-18 23:36:18 +0900

    view 작업 일부

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -3,18 +3,43 @@ import 'package:flutter/material.dart';
 class DefaultLayout extends StatelessWidget {
   final Widget child;
   final Color? backgroundColor;
+  final String? title;
+  final Widget? bottomNavigationBar;
 
   const DefaultLayout({
     Key? key,
     required this.child,
     this.backgroundColor,
+    this.title,
+    this.bottomNavigationBar,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppBar(),
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
+    );
+  }
+
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    }
+
+    return AppBar(
+      foregroundColor: Colors.black,
+      backgroundColor: Colors.white,
+      elevation: 0,
+      title: Text(
+        title!,
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
     );
   }
 }
